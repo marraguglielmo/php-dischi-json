9d392f02acaa2edab3161edd0963df2eacd60d96
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-04-17 13:10:36 +0200

    primo commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PHP Dischi JSON
+
+Dobbiamo creare una web-app che permetta di leggere una lista di dischi presente nel nostro server.
+
+### **Stack**
+
+Html, CSS, VueJS (importato tramite CDN), axios, PHP
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous' />
+
+    <!-- vue -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/3.4.23/vue.global.js' integrity='sha512-nG1pzAdB6onYkrCldpnkwHzZw7H3IUL4RLhinI7XEioaM8PHQDeHD6zPf+tog9WlKLAojc4zddvkTUpHvAZbew==' crossorigin='anonymous'></script>
+
+    <!-- axios -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.js' integrity='sha512-pmaEGtUAy7E5gRmwjLqQAD/QWw0dL4Z59b6b+2HQkqQwQ+HcPLLb9/KbUNo5zr10WuXmbivK9nYVZOyDkMgZMg==' crossorigin='anonymous'></script>
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>PHP Dischi JSON</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
