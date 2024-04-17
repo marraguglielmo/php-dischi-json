b7a2453e9a88c862bca0173afc3c07e8260d6677
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-04-17 17:15:23 +0200

    [add] info box

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ createApp({
     data(){
         return{
             apiUrl: 'server.php',
-            listaDischi : []
+            listaDischi : [],
+            activeId : 0,
+            showInfo: false
         }
     },
 
@@ -16,6 +18,13 @@ createApp({
                 this.listaDischi = res.data;
                 console.log(this.listaDischi);
             })
+        },
+
+        getInfoCard(){
+            this.showInfo = true;
+            // salvo in selectedDisc il disco attivo
+            const selectedDisc = this.listaDischi[this.activeId]
+            console.log(selectedDisc);
         }
     },
 
