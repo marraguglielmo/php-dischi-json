177643e99e3a0cc3db26574efcaa5479077e56c7
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-04-18 14:53:57 +0200

    [add] new disc PHP

--- server.php
+++ server.php
@@ -8,6 +8,26 @@ $json_string = file_get_contents('dischi.json');
 $list = json_decode($json_string, true);
 // var_dump($list);
 
+
+// verifico che in POST esista la variabile newDiscTitle
+// se esiste aggiungo un disco alla lista
+// aggiorno dischi.json
+
+if (isset($_POST['newDiscTitle'])) {
+    $new_disc = [
+        'title' => $_POST['newDiscTitle'],
+        'author' => $_POST['newDiscAuthor'],
+        'year' => $_POST['newDiscYear'],
+        'poster' => $_POST['newDiscPoster']
+
+    ];
+
+    $list[] = $new_disc;
+
+    // salvo la nuova lista trasformata in stringa
+    file_put_contents('dischi.json', json_encode($list));
+}
+
 // converto il file PHP in json
 header('Content-Type: application/json');
 
